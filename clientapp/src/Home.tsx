66562b78f5ead8c5e053627c3721e0de190426fa
rajasekar.hm <rajasekar.hm@zohocorp.com>
2024-08-13 21:51:54 +0530

    client blockchain added

--- clientapp/src/Home.tsx
+++ clientapp/src/Home.tsx
@@ -4,30 +4,30 @@ import { motion, MotionConfig, useMotionValue } from "framer-motion";
 import { Shapes } from "./Shapes";
 import { transition } from "./settings";
 import useMeasure, { RectReadOnly } from "react-use-measure";
-// import { Properties as CSSProperties } from 'cs';
+import connectThroughWindow from "./web3_actions/ethereum/connectWallet";
 
 const buttonStyle: any = {
-  '--purple': '#db07d1',
-  '--pink': '#f2056f',
-  '--blue': '#61dafb',
-  appearance: 'none',
-  border: 'none',
-  cursor: 'pointer',
-  backgroundColor: '#acc7ed',
-  color: '#fff',
-  borderRadius: '60px',
-  outline: 'none',
-  margin: '0',
-  padding: '12px 25px',
+  "--purple": "#db07d1",
+  "--pink": "#f2056f",
+  "--blue": "#61dafb",
+  appearance: "none",
+  border: "none",
+  cursor: "pointer",
+  backgroundColor: "#acc7ed",
+  color: "#fff",
+  borderRadius: "60px",
+  outline: "none",
+  margin: "0",
+  padding: "12px 25px",
   fontFamily: '"Poppins"',
-  fontSize: '48px',
-  fontWeight: '600',
-  lineHeight: '58px',
-  letterSpacing: '-1px',
-  position: 'relative',
-  textAlign: 'center',
-  display: 'flex',
-  alignItems: 'center'
+  fontSize: "48px",
+  fontWeight: "600",
+  lineHeight: "58px",
+  letterSpacing: "-1px",
+  position: "relative",
+  textAlign: "center",
+  display: "flex",
+  alignItems: "center",
 };
 
 export function Home() {
@@ -45,15 +45,24 @@ export function Home() {
   return (
     <MotionConfig transition={transition}>
       <motion.button
-        style={buttonStyle as React.CSSProperties}
+        onClick={connectThroughWindow}
+        style={
+          {
+            ...buttonStyle,
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            transform: "scale(0.8)",
+          } as React.CSSProperties
+        }
         ref={ref}
         initial={false}
         animate={isHover ? "hover" : "rest"}
         whileTap="press"
         variants={{
-          rest: { scale: 1 },
-          hover: { scale: 1.5 },
-          press: { scale: 1.4 }
+          rest: { scale: 0.5 },
+          hover: { scale: 0.8 },
+          press: { scale: 0.7 },
         }}
         onHoverStart={() => {
           resetMousePosition();
@@ -75,10 +84,63 @@ export function Home() {
           className="shapes"
           variants={{
             rest: { opacity: 0 },
-            hover: { opacity: 1 }
+            hover: { opacity: 1 },
           }}
         >
-          <Shapes isHover={isHover} isPress={isPress} mouseX={mouseX} mouseY={mouseY} />
+          <Shapes
+            isHover={isHover}
+            isPress={isPress}
+            mouseX={mouseX}
+            mouseY={mouseY}
+          />
+        </motion.div>
+        <motion.div
+          variants={{ hover: { scale: 0.5 }, press: { scale: 0.8 } }}
+          className="label"
+        >
+          Connect Wallet
+        </motion.div>
+      </motion.button>
+      <motion.button
+        style={buttonStyle as React.CSSProperties}
+        ref={ref}
+        initial={false}
+        animate={isHover ? "hover" : "rest"}
+        whileTap="press"
+        variants={{
+          rest: { scale: 1 },
+          hover: { scale: 1.5 },
+          press: { scale: 1.4 },
+        }}
+        onHoverStart={() => {
+          resetMousePosition();
+          setIsHover(true);
+        }}
+        onHoverEnd={() => {
+          resetMousePosition();
+          setIsHover(false);
+        }}
+        onTapStart={() => setIsPress(true)}
+        onTap={() => setIsPress(false)}
+        onTapCancel={() => setIsPress(false)}
+        onPointerMove={(e: React.PointerEvent<HTMLButtonElement>) => {
+          mouseX.set(e.clientX - bounds.x - bounds.width / 2);
+          mouseY.set(e.clientY - bounds.y - bounds.height / 2);
+        }}
+      >
+        <motion.div
+          className="shapes"
+          variants={{
+            rest: { opacity: 0 },
+            hover: { opacity: 1 },
+          }}
+        >
+          <Shapes
+            isHover={isHover}
+            isPress={isPress}
+            mouseX={mouseX}
+            mouseY={mouseY}
+          />
         </motion.div>
         <motion.div
           variants={{ hover: { scale: 0.85 }, press: { scale: 1.1 } }}
