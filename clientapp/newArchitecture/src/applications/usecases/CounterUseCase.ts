21db15f1aae155579a874713d3713fe5b2430155
rajasekar.hm <rajasekar.hm@zohocorp.com>
2024-10-11 18:54:45 +0530

    clean architecture code changes

--- clientapp/newArchitecture/src/applications/usecases/CounterUseCase.ts
+++ clientapp/newArchitecture/src/applications/usecases/CounterUseCase.ts
@@ -3,12 +3,25 @@ import AbstratUseCase from "./AbstractUseCase";
 import Counter from "./Counter";
 
 class CounterUseCase implements AbstratUseCase {
+    private counter: Counter;
+
+    constructor(counterEntity: CounterEntities) {
+        this.counter = new Counter(counterEntity); // Inject the entity
+    }
+
     public execute(entities: any): void {
         throw new Error("Method not implemented.");
     }
-    public increament( ) : void { 
-        const entities = new CounterEntities()
-        const counter = new Counter(0)
-        counter.increase(entities,1)
+
+    public increase(): number {
+        this.counter.increament(1); // Increment by 1
+        return this.counter["_counter"].get();
     }
-} 
+
+    public decrease(): number {
+        this.counter.decrement(1); // Decrement by 1
+        return this.counter["_counter"].get();
+    }
+}
+
+export default CounterUseCase
