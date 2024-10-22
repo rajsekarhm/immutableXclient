ef731f9d39f0615bcebe6a0f59085b5a9e190584
rajsekar <rajsekar.haribalan333@gmail.com>
2024-10-22 08:13:02 +0530

    architecture code changes

--- clientapp/src/architecture/applications/infrastructure/UserRepository.ts
+++ clientapp/src/architecture/applications/infrastructure/UserRepository.ts
@@ -4,6 +4,7 @@ import IUserRepository from '../../domains/repository/IUserRepository';
 class UserRepository implements IUserRepository{
     createUser(entity: UserEntity): void {
         // external interaction
+        console.log(entity)
         console.log('repository called')
     }
     findById(id: number | string): void {
