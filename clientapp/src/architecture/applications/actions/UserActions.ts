2b1652d5109ffedeca745230d3fc8294c1103d4d
rajasekar.hm <rajasekar.hm@zohocorp.com>
2024-11-05 21:39:45 +0530

    reducer actions  impl

--- clientapp/src/architecture/applications/actions/UserActions.ts
+++ clientapp/src/architecture/applications/actions/UserActions.ts
@@ -1,29 +1,41 @@
+import UserHandler from "../../controllers/handlers/UserHandler";
+import UserRepository from "../infrastructure/UserRepository";
+import UserUseCase from "../usecases/UserUseCase";
 import IAction from "./interface/Action";
 class UserAction implements IAction {
   constructor() {}
   actions() {
     return {
       users_create: this._create,
-      users_get:this._get,
-      users_update:this._update,
-      users_delete:this._delete
+      users_get: this._get,
+      users_update: this._update,
+      users_delete: this._delete,
+      users_login: this._login,
     };
   }
   _create(state: any, action: any) {
-    console.log('create user')
+    const userRepository = new UserRepository();
+    const userUseCase = new UserUseCase(userRepository);
+    const userController = new UserHandler(userUseCase);
+    console.log(state);
+    console.log("create user");
     // return { ...state, ...action.payload };
   }
   _get(state: any, action: any) {
-    console.log('get user')
+    console.log("get user");
     // return { ...state, ...action.payload };
   }
   _update(state: any, action: any) {
-    console.log('update user')
+    console.log("update user");
     // return { ...state, ...action.payload };
   }
   _delete(state: any, action: any) {
-    console.log('delete user')
+    console.log("delete user");
     // return { ...state, ...action.payload };
+  }
+  _login(state: any, action: any) {
+    console.log(state);
+    console.log("login user");
   }
 }
 export { UserAction };
