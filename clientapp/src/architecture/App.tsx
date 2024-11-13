1f0a24a9b358c9fbeabc78ec5e4da10ab47711d4
rajasekar.hm <rajasekar.hm@zohocorp.com>
2024-11-13 20:41:52 +0530

    sheet menu and drop down added

--- clientapp/src/architecture/App.tsx
+++ clientapp/src/architecture/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import Router from "./Router";
-import ResponsiveGrid from "./frameworks/components/Grid";
+// import { DropdownMenuDefault } from "./frameworks/components/DropMenu";
+
 export const App = () => {
-  return <>
-  {/* <ResponsiveGrid/> */}
-  <RouterProvider router={Router}/>
-  </>
+  return (
+    <>
+    {/* <DropdownMenuDefault/> */}
+      <RouterProvider router={Router}/>
+    </>
+  );
 };
 
 export default App;
---
+++ clientapp/src/architecture/frameworks/components/DropMenu.tsx
@@ -0,0 +1,110 @@
+import {
+    Cloud,
+    CreditCard,
+    Github,
+    Keyboard,
+    LifeBuoy,
+    LogOut,
+    Mail,
+    MessageSquare,
+    Plus,
+    PlusCircle,
+    Settings,
+    User,
+    UserPlus,
+    Users,
+  } from "lucide-react"
+  
+  import { Button } from "./shadcn/Button"
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+  } from "./shadcn/DropDown"
+  
+  type dropDown = {
+    content:string
+    isAction?:boolean
+    onAction?:any
+    icon?:any
+    isDiabled?:any
+    isSubMenu?:boolean
+    subMenuDetails?:any
+  }
+
+  export function DropdownMenuDefault({dropDownDetails}:any) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <User />
+              <span>Profile</span>
+              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <CreditCard />
+              <span>Billing</span>
+              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <Users />
+              <span>Team</span>
+            </DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <UserPlus />
+                <span>Invite users</span>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem>
+                    <Mail />
+                    <span>Email</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <MessageSquare />
+                    <span>Message</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <PlusCircle />
+                    <span>More...</span>
+                  </DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem disabled>
+            <Cloud />
+            <span>API</span>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <LogOut />
+            <span>Log out</span>
+            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+  }
+  
